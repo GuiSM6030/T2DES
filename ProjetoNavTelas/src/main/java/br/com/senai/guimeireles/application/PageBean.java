40373ab8a9cba51bda98930d95c3acbbaed2e4e2
aluno <aluno@DSN1251230716>
2022-04-12 17:04:21 -0300

    PageBean com condicional 12/04/2022

---
+++ ProjetoNavTelas/src/main/java/br/com/senai/guimeireles/application/PageBean.java
@@ -0,0 +1,34 @@
+package br.com.senai.guimeireles.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@SuppressWarnings("serial")
+@Named
+@RequestScoped
+public class PageBean implements Serializable{
+
+	private String page;
+
+	public String getPage() {
+		return page;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+
+	public String goTo() {
+		
+		if("a".equals(page)) {
+		return "page_a";
+	}else if("b".equals(page)){
+		return "page-b";
+	}else {
+		return null;
+
+		}
+	}
+}
