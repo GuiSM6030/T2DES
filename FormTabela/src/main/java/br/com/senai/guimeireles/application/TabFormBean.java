2ff852f7100b55ff99d0f2545d7c73ecf46847ff
aluno <aluno@DSN1251230716>
2022-05-20 14:23:53 -0300

    Agora, finalmente vai

--- FormTabela/src/main/java/br/com/senai/guimeireles/application/TabFormBean.java
+++ FormTabela/src/main/java/br/com/senai/guimeireles/application/TabFormBean.java
@@ -5,42 +5,90 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
-
 import javax.inject.Named;
 
-import br.com.senai.guimeireles.application.model.Dados;
+import br.com.senai.guimeireles.application.model.Cadastro;
 
 
-
-@SuppressWarnings("serial")
+@SuppressWarnings("serial") 
 @Named("tabela")
-@SessionScoped
+@SessionScoped 
 public class TabFormBean implements Serializable{
 	
-	private List<Dados> despesasList = new ArrayList<>();
+	private List<Cadastro> despesas = new ArrayList<>();
 	
-	public String inserir() {
-		Dados d = new Dados();
-		despesasList.add(d);
+	String date;
+	String desc;
+	Double valor;
+	Boolean back = false; 
+	
+	
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor1) {
+		valor = valor1;
+	}
+	
+	public String inserir(String data,String desc,Double valor) {
+		
+		Cadastro d = new Cadastro(data,desc,valor); 
 		d.setEdit(true);
+		back =true;
+		despesas.add(d);
+		date = null;
+		desc = null;
+		valor= null;
+		return null;
+		
+	}
+	
+	public String excluir(Cadastro despesa) {
+		
+		despesas.remove(despesa);
+		
 		return null;
 	}
 	
-	public String excluir(Dados dado) {
-		despesasList.remove(dado);
+	public String editar(Cadastro despesa) {
+		despesa.setEdit(true); 
+		
 		return null;
 	}
 	
-	public String editar(Dados dado) {
-		dado.setEdit(true);
+	public String gravar (Cadastro despesa) {
+		despesa.setEdit(false);
+		
 		return null;
 	}
 	
-	public String gravar(Dados dado) {
-		dado.setEdit(false);
-		return null;
+	public List<Cadastro> getDespesas() {
+		return despesas;
+	}
+
+	public Boolean getBack() {
+		return back;
+	}
+
+	public void setBack(Boolean back) {
+		this.back = back;
 	}
 
 }
-
-	
